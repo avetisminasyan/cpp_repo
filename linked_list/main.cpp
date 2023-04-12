ee9c2202e7ba0e105666a9a56675346fb3a4ee31
avetisminasyan <avominasyan71@gmail.com>
2023-04-12 14:18:00 +0400

    linked_list

--- linked_list/main.cpp
+++ linked_list/main.cpp
@@ -7,11 +7,9 @@ int main ()
 	assert(obj.is_empty()==true);
 	obj.push_back(2);
 	obj.push_back(5);
-	obj.push_back(3);
 	obj.push_back(78);
-	obj.insert(3,15);
-	/*obj.del(2);
-	obj.print();
+	obj.insert(2,15);
+	obj.del(2);
 	assert(obj.get_size() == 3);
 	obj.push_back(16);
 	assert(obj.get_size() == 4);
@@ -20,7 +18,7 @@ int main ()
 	obj.pop_back();
 	obj.pop_back();
 	obj.pop_back();
-	assert(obj.get_size() == 0);*/
+	assert(obj.get_size() == 0);
 	obj.print();
 	return 0;
 }
