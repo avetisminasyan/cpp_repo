5be5aec333f304566ed4a7483e9f1bf922bfa34d
avetisminasyan <avominasyan71@gmail.com>
2023-05-24 15:21:39 +0400

    fourbit

--- cpu/fourbit/add_test.v
+++ cpu/fourbit/add_test.v
@@ -4,12 +4,19 @@ reg [3:0] a,b;
 reg cin;
 wire[3:0] s;
 wire cout;
+
+wire [4:0] result;
+
+assign result = {cout, s};
+
 ADD4 t(a,b,cin,cout,s);
+
 initial begin
-	$monitor(s);
-	a = 7;
-	b = 5;
+	a = 12;
+	b = 12;
 	cin = 0;
+	#1;
+	$display("{s, cout} = %d", {cout, s});
 
 end
 endmodule
